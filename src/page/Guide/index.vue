2e3fc4c372d85d5542c27a06fbdb719987fa925f
ch <chenhuan1@akulaku.com>
2021-12-03 12:13:45 +0800

    feat: project init

---
+++ src/page/Guide/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="guide">
+    引导页
+    <span>hxf love ni</span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.guide {
+  span {
+    color: red;
+  }
+}
+</style>
