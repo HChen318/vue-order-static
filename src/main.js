3c9ab00ac474519e66accef6c80bbed4b624f725
ch <chenhuan1@akulaku.com>
2021-12-04 11:18:39 +0800

    feat: init

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vant from 'vant';
+import '../config/rem'
 import 'normalize.css/normalize.css'
 import 'vant/lib/index.css';
 
--- src/page/Guide/index.vue
+++ src/page/Guide/index.vue
@@ -6,7 +6,18 @@
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            key: value
+        }
+    },
+    methods: {
+        name() {
+            
+        }
+    },
+};
 </script>
 
 <style lang="less" scoped>
